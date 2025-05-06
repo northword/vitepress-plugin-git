9630575cc8ed60dee68f2ce32dea7a77a8d775e6
Northword <northword@outlook.com>
2025-05-06 18:50:53 +0800

    chore: update build config

--- build.config.ts
+++ build.config.ts
@@ -7,31 +7,22 @@ const allExternal = [
 ]
 
 export default defineBuildConfig({
-  // entries: ['./src/'],
   entries: [
-    { builder: 'mkdist', input: './src/client', outDir: './dist/client', pattern: ['**/*.vue'], loaders: ['vue'] },
-    { builder: 'mkdist', input: './src/client', outDir: './dist/client', pattern: ['**/*.ts'], format: 'esm', loaders: ['js'], ext: 'js' },
-    { builder: 'mkdist', input: './src/shared', outDir: './dist/shared', pattern: ['**/*.ts'], format: 'esm', loaders: ['js'], ext: 'js' },
+    // bundless for client, because Vue SFC needs file structure
+    { input: './src/client/', outDir: './dist/client' },
+    // mkdist for shared, because we have some .d.ts files cannot be bundle
+    { input: './src/shared/', outDir: './dist/shared' },
+    // bundle for node
     { builder: 'rollup', input: './src/index', outDir: './dist' },
   ],
   clean: true,
-  sourcemap: true,
   declaration: true,
   rollup: {
-    emitCJS: true,
-    inlineDependencies: true,
+    // inlineDependencies: true,
   },
   externals: [
-    'fs',
-    'path',
-    'os',
-    'child_process',
-    'events',
-    'stream',
-    'fsevents',
     'vite',
     'vitepress',
-    'vite-plugin-git',
     ...allExternal,
   ],
 })
