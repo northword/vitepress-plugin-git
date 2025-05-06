b3ead4e59cb4b608d113cae370b2e42d9cde7239
Northword <northword@outlook.com>
2025-05-06 21:47:39 +0800

    revert: update build config

--- build.config.ts
+++ build.config.ts
@@ -9,15 +9,19 @@ const allExternal = [
 export default defineBuildConfig({
   entries: [
     // bundless for client, because Vue SFC needs file structure
-    { input: './src/client/', outDir: './dist/client' },
+    // { input: './src/client/', outDir: './dist/client' },
+    { builder: 'mkdist', input: './src/client', outDir: './dist/client', pattern: ['**/*.vue'], loaders: ['vue'] },
+    { builder: 'mkdist', input: './src/client', outDir: './dist/client', pattern: ['**/*.ts'], format: 'esm', loaders: ['js'], ext: 'js' },
     // mkdist for shared, because we have some .d.ts files cannot be bundle
-    { input: './src/shared/', outDir: './dist/shared' },
+    // { input: './src/shared/', outDir: './dist/shared' },
+    { builder: 'mkdist', input: './src/shared', outDir: './dist/shared', pattern: ['**/*.ts'], format: 'esm', loaders: ['js'], ext: 'js' },
     // bundle for node
     { builder: 'rollup', input: './src/index', outDir: './dist' },
   ],
   clean: true,
   declaration: true,
   rollup: {
+    emitCJS: true,
     // inlineDependencies: true,
   },
   externals: [
