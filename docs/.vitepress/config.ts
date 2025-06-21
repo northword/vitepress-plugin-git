43b5951276a346b8855715515daaf30e79630613
Northword <northword@outlook.com>
2025-06-21 11:21:25 +0800

    docs: tweaks

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -12,35 +12,48 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/northword/vitepress-plugin-git' },
     ],
-    sidebar: [
-      {
-        text: 'Guide',
-        items: [
-          { text: 'Getting Started', link: '/getting-started' },
-          { text: 'Config', link: '/options' },
-          { text: 'Secondary Development', link: '/secondary-development' },
-        ],
-      },
-      // {
-      //   text: 'Example',
-      //   items: [
-      //     { text: 'Default', link: '/example/index' },
-      //     { text: 'Disable Contributors', link: '/example/disable-contributors' },
-      //     { text: 'Disable Changelog', link: '/example/disable-changelog' },
-      //     { text: 'Chinese', link: '/example/lang-zh' },
-      //   ],
-      // },
-    ],
-
   },
   locales: {
     root: {
       label: 'English',
       lang: 'en-US',
+      themeConfig: {
+        sidebar: [
+          {
+            text: 'Guide',
+            items: [
+              { text: 'Getting Started', link: '/getting-started' },
+              { text: 'Config', link: '/options' },
+              { text: 'Secondary Development', link: '/secondary-development' },
+            ],
+          },
+          // {
+          //   text: 'Example',
+          //   items: [
+          //     { text: 'Default', link: '/example/index' },
+          //     { text: 'Disable Contributors', link: '/example/disable-contributors' },
+          //     { text: 'Disable Changelog', link: '/example/disable-changelog' },
+          //     { text: 'Chinese', link: '/example/lang-zh' },
+          //   ],
+          // },
+        ],
+      },
     },
     zh: {
       label: '简体中文',
       lang: 'zh-CN',
+      themeConfig: {
+        sidebar: [
+          {
+            text: 'Guide',
+            items: [
+              { text: '快速上手', link: '/getting-started' },
+              { text: '设置插件', link: '/options' },
+              { text: '二次开发', link: '/secondary-development' },
+            ],
+          },
+        ],
+      },
     },
   },
 
